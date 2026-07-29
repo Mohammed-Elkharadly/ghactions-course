bd7a3502ecb5937c7c3154d0dc81d71228927fdc
Mohammed_elkharadly <mkoko201540@gmail.com>
2026-07-30 00:39:43 +0300

    update some files

--- src/test.sh
+++ src/test.sh
@@ -3,18 +3,12 @@
 #src/test.sh
 EXPECTED="hello john"
 
-OUTPUT=$(node -e "console.log(require('/src/app')('john'))")
+OUTPUT=$(node -e "console.log(require('./src/app')('john'))")
 
-if [ "$OUTPUT" == "$EXPECTED"]; then
-
-echo "test passed"
-
-exit 0
-
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+  echo "✅ test passed"
+  exit 0
 else 
-
-echo "test failed expected '$EXPECTED' but got '$OUTPUT'"
-
-exit 1
-
+  echo "❌ test failed expected '$EXPECTED' but got '$OUTPUT'"
+  exit 1
 fi
