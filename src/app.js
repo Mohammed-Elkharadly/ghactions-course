f6b128581b1e622c86e4adee436c2ca46788e1ea
Mohammed_elkharadly <mkoko201540@gmail.com>
2026-07-30 00:46:56 +0300

    test changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function great(name) {
   return `hello ${name}`;
 }
-if (require.main === module) {
-  console.log(great("john"));
-}
+// if (require.main === module) {
+//   console.log(great("john"));
+// }
 module.exports = great;
